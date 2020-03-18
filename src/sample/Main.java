486d52dc429babf3d49bfa4087fbec8e8af4c3e2
Michael <mjs.namand@gmail.com>
2020-03-19 00:16:50 +0100

    FIX Main.java

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,28 +1,11 @@
 package sample;
 
-import graphe.Graphe;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuItem;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.FlowPane;
-import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-    static Graphe graphe = null;
     @Override
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("Graphes");
