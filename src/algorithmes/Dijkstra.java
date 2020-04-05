b740a2a0a6b4c5876c3fb252e821ad4424e79882
AmB974 <ambriii.mussard@gmail.com>
2020-04-05 02:30:20 +0200

    FIX Dijkstra

--- src/algorithmes/Dijkstra.java
+++ src/algorithmes/Dijkstra.java
@@ -11,101 +11,93 @@ public class Dijkstra {
 
     //int s, int[] fs, int[] aps, int[][] couts, int[] d, int[] pred
 
-    public static void dijkstra(Graphe graphe) {
-        int INT_MAX=10;
+    public static boolean[] dijkstra(Graphe graphe, int sommets) {
+       boolean[] visite = new boolean[graphe.getSommets().size()];      //tabeleau pour savoir si le sommet a était visité
+       int[] etiquette = new int[graphe.getSommets().size()];           //le cout du chemin le plus rapide a partir du sommet choisis
+       int tmps_score=0;
+       int score = 0;
+       boolean parcour__fini = false;
+       int tmp_sommet= sommets;
+       int score_comparaison = 0;
+       Sommet sommet_second ;
+        Sommet sommet_sauvegarder = null;
 
-        //initialisation variable
-        int[][] fsAps = graphe.getFsAps();
-        int[] fs = fsAps[0];
-        int[] aps = fsAps[1];
-        int n = aps[0];
 
 
+       //initialisation du tableau de visiste, tous non visité
+        //initialisation du tableau de etiquette, tous na -1 quand in sont pas d'accés du sommet choisis
+       for(int i = 0; i < graphe.getSommets().size();i++){
+           visite[i]=false;
+           etiquette[i]=-1;
+       }
 
-            int[][] couts = new int[graphe.getSommets().size()][graphe.getSommets().size()];
-            int[] d;
-            int[] pred;
+       //initialiser les cases deja visité
+       etiquette[sommets] = score;
+       visite[sommets]= true;
 
-            //initialisation tableau de cout
-            for (int i = 0; i < graphe.getSommets().size(); i++) {
-                for (int j = 1; j < graphe.getSommets().size(); j++)
-                {
-                    //System.out.println(graphe.getArcFromSommets(graphe.getSommet(i), graphe.getSommet(j)).getCout());
 
-                    System.out.println("init" + i);
-                    couts[i][j] = -1;
-                    System.out.println("init apres" + i);
-                }
-            }
+       // parcourir les successeur jusqu a que ca soit fini
+       while(parcour__fini==false){
 
-        for(int sommet=0; sommet<graphe.getSommets().size();sommet++) {
-            d = new int[n + 1]; // Maintient l'information sur la plus petite distance reliant un sommet de S vers X (plus petit pont)
-            pred = new int[n + 1];
-            boolean[] inS = new boolean[n + 1]; // Renseigne à chaque étape si le sommet i est dans S ou non.
-            // Initialisation du processus
-            for (int i = 1; i <= n; i++) {
-                d[i] = couts[sommet][i];
-                inS[i] = false;
-                if (d[i] != INT_MAX) {
-                    pred[i] = sommet;
-                } else {
-                    pred[i] = -1;
-                }
-            }
-            
-            inS[sommet] = true;
-            int cpt = n - 1;
-            while (cpt > 0) {
-                int j = dmin(d, inS); // Sommet recherché. Case minimum n'appartenant pas à S
-                if (j == -1) {
-                    return; // Il ne reste plus de sommet accessible à partir de S
-                }
-                inS[j] = true;
-                int k; // Successeur non traité (absents de S)
-                for (int p = aps[j]; (k = fs[p]) != 0; p++) {
-                    if (!inS[k]) {
-                        // On compare l'ancienne valeure de d pour k
-                        // On en tient compte uniquement si d[j] et son cout de j vers k différents de +∞
-                        int v; // Valeur du chemin le plus court pour aller vers k passant par j
-                        if (couts[j][k] != INT_MAX) {
-                            v = d[j] + couts[j][k];
-                            if (v < d[k]) {
-                                d[k] = v;
-                                pred[k] = j;
-                            }
-                        }
-                    }
-                }
-                cpt--;
+           //si il y a des successeur
+           if(graphe.getSuccesseursSommet(graphe.getSommet(tmp_sommet)).size()!=0){
+               for(int l = 0; l<graphe.getSuccesseursSommet(graphe.getSommet(tmp_sommet)).size();l++){
+                   //sommet du successeur
+                   sommet_second =graphe.getSuccesseursSommet(graphe.getSommet(tmp_sommet)).get(l);
+                   //cout de l'arc
+                   tmps_score = graphe.getArcFromSommets(graphe.getSommet(tmp_sommet), sommet_second).getCout();
 
-            }
-            System.out.println("sommet: "+ sommet);
-        }
+                   //si sommet du départ egal second sommet (point de départ)
+                   if(sommet_second.id()== sommets ) {
 
-    }
+                       //si c'est le dernier des succeseurs
+                       if(l == graphe.getSuccesseursSommet(graphe.getSommet(tmp_sommet)).size()){
+                           parcour__fini=true; // finir la recherche, c'est le dernier des dernier
+                       }
+                       //sinon on ne fait rien
 
-    public static int dmin(int[] d, boolean[] inS){
-        int i=0;
+                   } else {// si c'est pas egal au sommet
 
-        while ( i < inS.length && inS[i]){
-            i++;
-            
-        }
+                       //si c'est pas le premier de la boucle
+                       if (l != 0) {
+                           //Si l'initialisation est suprieur
+                           if (score_comparaison > score + tmps_score && sommet_second.id() != sommets) {
+                              //on re-initialise
+                               score_comparaison = score + tmps_score;
+                               sommet_sauvegarder = sommet_second;
+                           }
 
-        return d[i];
-    }
+                       } else {//initialisé la comparaison
+                           score_comparaison = score + tmps_score;
+                           sommet_sauvegarder = sommet_second;
+                       }
 
-    /*public static void main(String[] args) {
-        Dijkstra di = new Dijkstra();
-        Graphe gr = new Graphe("lool");
-        Sommet s1= new Sommet(1, "s1", gr);
-        Sommet s2= new Sommet(2, "s2", gr);
-        Arc a1 = new Arc(10, s1,s2,gr);
-        di.dijkstra(gr);
+                   }
 
+               }//fin for
+
+               //si on est pas a la fin
+               if(parcour__fini==false) {
+                   score = score_comparaison;
+                   etiquette[sommet_sauvegarder.id()] = score;
+                   visite[sommet_sauvegarder.id()] = true;
+
+                   tmp_sommet = sommet_sauvegarder.id();
+               }
+
+
+           } else {
+                parcour__fini=true; //fini de remplire de tableau
+           }
+
+       }
+
+
+        return visite;
 
     }
-    */
+
+
 
 
 }
